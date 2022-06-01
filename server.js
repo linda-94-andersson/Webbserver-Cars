2d2000e184e1879c31b78e8c2553a383eb702f76
Linda Andersson <linda.29.andersson@gmail.com>
2022-06-01 13:37:13 +0200

    Setup for cars

--- server.js
+++ server.js
@@ -9,13 +9,16 @@ const PORT = process.env.PORT || 4000
 const app = express();
 
 let owners = [];
+let cars = [];
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
 const ownersRouter = express.Router();
+const carsRouter = express.Router();
 
+// owner 
 ownersRouter.get("/owners", (req, res) => {
     res.json(owners);
 })
@@ -41,11 +44,40 @@ ownersRouter.post("/owners", (req, res) => {
 
 ownersRouter.delete("/owners/:id", (req, res) => {
     owners = owners.filter((owner) => owner.id !== req.params.id);
-    console.log(owners); 
     res.json(owners);
 })
 
+//cars
+carsRouter.get("/cars", (req, res) => {
+    res.json(cars);
+})
+
+carsRouter.get("/cars/:id", (req, res) => {
+    const foundCar = cars.find((car) => car.id === req.params.id);
+    res.json(foundCar);
+})
+
+carsRouter.post("/cars", (req, res) => {
+    if (!req.body.reg) {
+        return res.status(400).json({ error: "Reg.nummer saknas" });
+    }
+
+    cars.push({
+        id: uuid.v4(),
+        reg: req.body.reg
+    })
+
+    const reg = req.body.reg;
+    res.send(reg);
+})
+
+carsRouter.delete("/cars/:id", (req, res) => {
+    cars = cars.filter((car) => car.id !== req.params.id);
+    res.json(cars);
+})
+
 app.use(ownersRouter);
+app.use(carsRouter);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
